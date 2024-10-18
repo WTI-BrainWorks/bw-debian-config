929419094f659f993c43b03483151811e9864d5e
Alex Forrence <alexander.forrence@yale.edu>
2024-10-18 10:38:32 -0400

    fix the env generators, fix firefox settings

--- config/includes.chroot/etc/firefox-esr/syspref.js
+++ config/includes.chroot/etc/firefox-esr/syspref.js
@@ -1,7 +1,6 @@
-let blank = "about:blank";
-pref("browser.startup.homepage", blank);
-pref("startup.homepage_welcome_url", blank);
-pref("startup.homepage_welcome_url.additional", blank);
+pref("browser.startup.homepage", "about:blank");
+pref("startup.homepage_welcome_url", "about:blank");
+pref("startup.homepage_welcome_url.additional", "about:blank");
 // disable first welcome
 pref("browser.aboutwelcome.enabled", false);
 pref("trailhead.firstrun.branches", "nofirstrun-empty");
